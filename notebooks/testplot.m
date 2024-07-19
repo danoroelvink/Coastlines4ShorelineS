c8d2dd5012a101878cdb65023e03106bd18eade8
danoroelvink <d.roelvink@unesco-ihe.org>
2024-07-19 16:18:18 +0200

    Working example for Sanibel and Captiva
    create netcdf file with ok shorleines for Sanibel and Captiva. Matlab script fills missing time points by interpolation

--- notebooks/testplot.m
+++ notebooks/testplot.m
@@ -1,11 +1,35 @@
 clear all;close all
+nmin=30;
 lon=ncread("test.nc",'lon');
 lat=ncread("test.nc",'lat');
-ok=sum(isnan(lon),2)<30;
+%% 
+ok=sum(isnan(lon),2)<nmin;
 lon=lon(ok,:);
 lat=lat(ok,:);
 time=double(ncread("test.nc",'time')+datenum(1984,1,1));
 time(1)=nan;
+timok=~isnan(time);
+time=time(timok);
+lon=lon(:,timok);
+lat=lat(:,timok);
+%% Fill in gaps by interpolation in time
+for i=1:size(lon,1)
+    loclon=lon(i,:);
+    loclat=lat(i,:);
+    valid=~isnan(loclon);
+    vallon=loclon(valid);
+    vallat=loclat(valid);
+    valtime=time(valid);
+    lon(i,:)=interp1(valtime,vallon,time,"linear","extrap");
+    lat(i,:)=interp1(valtime,vallat,time,"linear","extrap");
+end
+nans=time'*nan;
+dist=hypot(diff(lon(:,end)),diff(lat(:,end)));
+id=find(dist>0.02);
+for j=length(id):-1:1
+    lon=[lon(1:id(j),:);nans;lon(id(j)+1:end,:)];
+    lat=[lat(1:id(j),:);nans;lat(id(j)+1:end,:)];
+end
 year=datevec(time);
 year=year(:,1);
 c=colormap("parula")
